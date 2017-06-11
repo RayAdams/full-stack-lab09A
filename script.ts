efd3e75b90e7d3ca0e03af10759843bbd69d1bdd
RayAdams <cramerrachael@gmail.com>
2017-06-10 23:23:12 -0400

    updated ts/removed errors

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-let diceArray: number[] = [];
+let diceArray: Die[] = [];
 
 //constructor
 class Die {
@@ -6,36 +6,36 @@ class Die {
     div: HTMLDivElement;
 
     constructor() {
-    this.value = this.roll();
-    this.div = document.createElement('div');
-    this.div.className = 'die';
-    this.div.innerHTML = String(this.value);
-    this.div.addEventListener('click', this.roll);
-    this.div.addEventListener('dblclick', this.removeDie);
-    document.body.appendChild(this.div);
+        this.value = this.roll();
+        this.div = document.createElement('div');
+        this.div.className = 'die';
+        this.div.innerHTML = String(this.value);
+        document.body.appendChild(this.div);
+        this.div.addEventListener('click', () => {
+            this.value = this.roll()
+            this.div.innerHTML = String(this.value);
+        });
+        //removes double clicked div
+        this.div.addEventListener('dblclick', () => {
+            this.div.remove();
+            diceArray.splice(diceArray.indexOf(<any>this), 1);
+        })
     }
     //random assigns 1-6 to value and updates the div
     roll() : number {
         return Math.ceil(Math.random() * 6);
     }
 
-    //removes double clicked div
-    removeDie():  {
-        this.div.remove();
-        var index = diceArray.indexOf(this);
-        diceArray.splice(index, 1);
-    }
 
 }
 
+//die create on click
 document.getElementById('create').addEventListener('click', () => {
-    //longer way to write:  var d = new Die();
     diceArray.push(new Die());
 });
 
-//reroll the dice values
-let rollBtn = document.getElementById('roll');
-rollBtn.addEventListener('click', () => {
+//reroll the dice values;
+document.getElementById('roll').addEventListener('click', () => {
     for (let i = 0; i < diceArray.length; i++){
         diceArray[i].value = diceArray[i].roll();
         diceArray[i].div.innerHTML = String(diceArray[i].value);
